c33eb92f84c747c2ccc98b8914d99c10bbcc2e60
Timmothy Rain <ruben.p960@stu.nebo.edu>
2023-10-03 23:28:17 +0000

    Update 5 files
    - /boards/b/b.html
    - /script.js
    - /boards/mu/music.html
    - /boards/b/random.html
    - /boards/b/b.js

--- boards/b/b.js
+++ boards/b/b.js
@@ -1,7 +1,4 @@
-// PS! Replace this with your own channel ID
-// If you use this channel ID your app will stop working in the future
-// const CLIENT_ID = '4cNswoNqM2wVFHPg';
-const CLIENT_ID = 'uqTyASJn6WnbK8wJ';
+const CLIENT_ID = 'RKfsdDmmQWAB060u';
 
 const drone = new ScaleDrone(CLIENT_ID, {
   data: { // Will be sent out as clientData via events
