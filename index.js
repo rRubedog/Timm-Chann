7f5cbbf822a71aa551440ed03c740c066a25fb11
Timmothy Rain <ruben.p960@stu.nebo.edu>
2023-10-04 02:37:00 +0000

    Update 2 files
    - /index.html
    - /index.js

---
+++ index.js
@@ -0,0 +1,88 @@
+const bBoardID = new Scaledrone('RKfsdDmmQWAB060u');
+const muBoardID = new Scaledrone('eod9jEwBJN8RbLSs');
+
+bBoardID.on('open', error => {
+if (error) {
+    return console.error(error);
+}
+console.log('Successfully connected to /b/');
+
+const bRoom = bBoardID.subscribe('observable-room', {
+    historyCount: 20
+});
+bRoom.on('open', error => {
+    if (error) {
+    return console.error(error);
+    }
+    console.log('Successfully joined /b/');
+});
+bRoom.on('history_message', message => addHistory(message));
+
+function addHistory(message){
+    const le = const le = document.querySelector('.all-chats');
+    const hdata = message.data;
+    const name = 'Anonymous';
+    const color = '#295523';
+    // member element
+    const pastMember = document.createElement('div');
+    pastMember.appendChild(document.createTextNode(name));
+    pastMember.className = 'member';
+    pastMember.style.color = color;
+    // message text element
+    const pastMessageData = document.createTextNode(hdata);
+    // full message element
+    const pastMessage = document.createElement('div');
+    pastMessage.appendChild(pastMember);
+    pastMessage.appendChild(pastMessageData);
+    pastMessage.className = 'message';
+    // append
+    const wasTop = le.scrollTop === le.scrollHeight - le.clientHeight;
+    le.appendChild(pastMessage);
+    if (wasTop) {
+        le.scrollTop = le.scrollHeight - le.clientHeight;
+    }
+}
+});
+
+muBoardID.on('open', error => {
+    if (error) {
+        return console.error(error);
+    }
+    console.log('Successfully connected to /mu/');
+    
+    const muRoom = bBoardID.subscribe('observable-room', {
+        historyCount: 20
+    });
+    muRoom.on('open', error => {
+        if (error) {
+        return console.error(error);
+        }
+        console.log('Successfully joined /mu/');
+    });
+    muRoom.on('history_message', message => addHistory(message));
+    
+    function addHistory(message){
+        const le = document.querySelector('.all-chats');
+        const hdata = message.data;
+        const name = 'Anonymous';
+        const color = '#295523';
+        // member element
+        const pastMember = document.createElement('div');
+        pastMember.appendChild(document.createTextNode(name));
+        pastMember.className = 'member';
+        pastMember.style.color = color;
+        // message text element
+        const pastMessageData = document.createTextNode(hdata);
+        // full message element
+        const pastMessage = document.createElement('div');
+        pastMessage.appendChild(pastMember);
+        pastMessage.appendChild(pastMessageData);
+        pastMessage.className = 'message';
+        // append
+        const wasTop = le.scrollTop === le.scrollHeight - le.clientHeight;
+        le.appendChild(pastMessage);
+        if (wasTop) {
+            le.scrollTop = le.scrollHeight - le.clientHeight;
+        }
+    }
+    });
