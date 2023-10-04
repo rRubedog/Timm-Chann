9fc8495484be6cf30063af4bef19b4b4594157e0
Timmothy Rain <ruben.p960@stu.nebo.edu>
2023-10-04 00:39:47 +0000

    Update 3 files
    - /boards/b/b.js
    - /boards/b/random.html
    - /boards/b/b.css

--- boards/b/b.js
+++ boards/b/b.js
@@ -132,7 +132,7 @@ function createMemberElement(member) {
 }
 
 function updateMembersDOM() {
-  DOM.membersCount.innerText = `${members.length} users in room:`;
+  DOM.membersCount.innerText = `${members.length} users in room`;
   // DOM.membersList.innerHTML = '';
   // members.forEach(member =>
   //   DOM.membersList.appendChild(createMemberElement(member))
