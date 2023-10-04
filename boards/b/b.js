99e9651de96a4539f1f49e54ff85022e6e2eaea2
Timmothy Rain <ruben.p960@stu.nebo.edu>
2023-10-04 00:43:39 +0000

    Update file b.js

--- boards/b/b.js
+++ boards/b/b.js
@@ -133,6 +133,7 @@ function createMemberElement(member) {
 
 function updateMembersDOM() {
   DOM.membersCount.innerText = `${members.length} users in room`;
+  document.querySelector('side-count').innerHTML = `${members.length} users in room`;
   // DOM.membersList.innerHTML = '';
   // members.forEach(member =>
   //   DOM.membersList.appendChild(createMemberElement(member))
