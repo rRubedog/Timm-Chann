94949c7a6b72d24a680ac5974f39ecd4e0bfaa41
Timmothy Rain <ruben.p960@stu.nebo.edu>
2023-10-11 01:48:33 +0000

    Update file index.js

--- index.js
+++ index.js
@@ -110,22 +110,23 @@ boardID.on('open', error => {
   c.on('member_join', member => {
     members.push(member);
     membersCount += 1;
-    document.querySelector('.user').innerText = membersCount + ' users online';
+    document.querySelector('.user').innerText = membersCount + ' total users online';
   });
 
   c.on('member_leave', ({id}) => {
     const index = members.findIndex(member => member.id === id);
     members.splice(index, 1);
     membersCount -= 1;
-    document.querySelector('.user').innerText = membersCount + ' users online';
+    document.querySelector('.user').innerText = membersCount + ' total users online';
   });
 
-  b.unsubscribe();
-  mu.unsubscribe();
-  c.unsubscribe();
-  // membersCount -= 3;
-  document.querySelector('.user').innerText = membersCount + ' users online';
 });
+
+b.unsubscribe();
+mu.unsubscribe();
+c.unsubscribe();
+
+document.querySelector('.user').innerText = membersCount + ' total users online';
 
 boardID.on('close', event => {
   console.log('Connection was closed', event);
