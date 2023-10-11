1aa9fefe6ecb59e6a0f2e0f330c7583e9533e502
Timmothy Rain <ruben.p960@stu.nebo.edu>
2023-10-11 00:57:57 +0000

    Update file index.js

--- index.js
+++ index.js
@@ -1,74 +1,74 @@
 const boardID = new Scaledrone('RKfsdDmmQWAB060u');
 
 boardID.on('open', error => {
-if (error) {
+  if (error) {
     return console.error(error);
-}
-console.log('Successfully connected to /b/');
-// /b/
-const b = boardID.subscribe('observable-b', {
+  }
+  console.log('Successfully connected to /b/');
+  // /b/
+  const b = boardID.subscribe('observable-b', {
     historyCount: 5
-});
-b.on('open', error => {
+  });
+  b.on('open', error => {
     if (error) {
     return console.error(error);
     }
     console.log('Successfully joined /b/');
-});
-const bl = document.querySelector('.b-chats');
-b.on('history_message', message => baddHistory(message, bl));
+  });
+  const bl = document.querySelector('.b-chats');
+  b.on('history_message', message => baddHistory(message, bl));
 
-// /mu/
-const mu = boardID.subscribe('observable-mu', {
-  historyCount: 5
-});
-mu.on('open', error => {
-  if (error) {
-  return console.error(error);
+  // /mu/
+  const mu = boardID.subscribe('observable-mu', {
+    historyCount: 5
+  });
+  mu.on('open', error => {
+    if (error) {
+    return console.error(error);
+    }
+    console.log('Successfully joined /mu/');
+  });
+  const mul = document.querySelector('.mu-chats');
+  mu.on('history_message', message => baddHistory(message, mul));
+
+  // /c/
+  const c = boardID.subscribe('observable-c', {
+    historyCount: 5
+  });
+  c.on('open', error => {
+    if (error) {
+      return console.error(error);
+    }
+    console.log('Successfully joined /c/');
+  });
+  const cl = document.querySelector('.c-chats');
+  c.on('history_message', message => baddHistory(message, cl));
+
+
+
+  function addHistory(message, el){
+    const data = message.data;
+    const name = 'Anonymous';
+    const color = '#295523';
+    // member element 
+    const pastMember = document.createElement('div');
+    pastMember.appendChild(document.createTextNode(name));
+    pastMember.className = 'member';
+    pastMember.style.color = color;
+    // message text element
+    const pastMessageData = document.createTextNode(data);
+    // full message element
+    const pastMessage = document.createElement('div');
+    pastMessage.appendChild(pastMember);
+    pastMessage.appendChild(pastMessageData);
+    pastMessage.className = 'message';
+    // append
+    const wasTop = el.scrollTop === el.scrollHeight - el.clientHeight;
+    el.appendChild(pastMessage);
+    if (wasTop) {
+      el.scrollTop = el.scrollHeight - el.clientHeight;
+    }
   }
-  console.log('Successfully joined /mu/');
-});
-const mul = document.querySelector('.mu-chats');
-mu.on('history_message', message => baddHistory(message, mul));
-
-// /c/
-const c = boardID.subscribe('observable-c', {
-  historyCount: 5
-});
-c.on('open', error => {
-  if (error) {
-  return console.error(error);
-  }
-  console.log('Successfully joined /c/');
-});
-const cl = document.querySelector('.c-chats');
-c.on('history_message', message => baddHistory(message, cl));
-
-
-
-function addHistory(message, el){
-  const data = message.data;
-  const name = 'Anonymous';
-  const color = '#295523';
-  // member element 
-  const pastMember = document.createElement('div');
-  pastMember.appendChild(document.createTextNode(name));
-  pastMember.className = 'member';
-  pastMember.style.color = color;
-  // message text element
-  const pastMessageData = document.createTextNode(data);
-  // full message element
-  const pastMessage = document.createElement('div');
-  pastMessage.appendChild(pastMember);
-  pastMessage.appendChild(pastMessageData);
-  pastMessage.className = 'message';
-  // append
-  const wasTop = el.scrollTop === el.scrollHeight - el.clientHeight;
-  el.appendChild(pastMessage);
-  if (wasTop) {
-    el.scrollTop = el.scrollHeight - el.clientHeight;
-  }
-}
 
 // /b/
   b.on('members', m => {
