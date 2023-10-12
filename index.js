e5ceba99d8d2ba8368233e8c92b384fd926f89d3
Timmothy Rain <ruben.p960@stu.nebo.edu>
2023-10-12 02:36:22 +0000

    Update file index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ boardID.on('open', error => {
     console.log('Successfully joined /b/');
   });
   const bl = document.querySelector('.b-chats');
-  b.on('history_message', message => addHistory(message, bl, b));
+  b.on('history_message', message => addHistory(message, bl, b, '0000ff'));
 
   // /mu/
   const mu = boardID.subscribe('observable-mu', {
@@ -30,7 +30,7 @@ boardID.on('open', error => {
     console.log('Successfully joined /mu/');
   });
   const mul = document.querySelector('.mu-chats');
-  mu.on('history_message', message => addHistory(message, mul, mu));
+  mu.on('history_message', message => addHistory(message, mul, mu, '800080'));
 
   // /c/
   const c = boardID.subscribe('observable-c', {
@@ -43,12 +43,12 @@ boardID.on('open', error => {
     console.log('Successfully joined /c/');
   });
   const cl = document.querySelector('.c-chats');
-  c.on('history_message', message => addHistory(message, cl, c));
+  c.on('history_message', message => addHistory(message, cl, c, 'dc143c'));
 
-  function addHistory(message, el, board){
+  function addHistory(message, el, board, boardC){
     const data = message.data;
     const name = 'Anonymous';
-    const color = '#295523';
+    const boardC = "#" + boardC;
     // member element 
     const pastMember = document.createElement('div');
     pastMember.appendChild(document.createTextNode(name));
