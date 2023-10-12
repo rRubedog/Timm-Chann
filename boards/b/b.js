6ed35cc154039b18697c6fa95a2fb894ab333e5c
Timmothy Rain <ruben.p960@stu.nebo.edu>
2023-10-12 02:22:30 +0000

    Update 2 files
    - /boards/b/b.js
    - /styles/style.css

--- boards/b/b.js
+++ boards/b/b.js
@@ -30,7 +30,7 @@ drone.on('open', error => {
     const le = DOM.messages;
     const hdata = message.data;
     const name = 'Anonymous';
-    const color = '#800000';
+    const color = '#ff3300';
     // member element
     const pastMember = document.createElement('div');
     pastMember.appendChild(document.createTextNode(name));
