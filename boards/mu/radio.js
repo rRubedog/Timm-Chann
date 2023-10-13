175db4fab133ff47cf988af0365b2a1203494911
Timmothy Rain <ruben.p960@stu.nebo.edu>
2023-10-13 18:16:12 +0000

    Update 2 files
    - /boards/mu/music.html
    - /boards/mu/radio.js

--- boards/mu/radio.js
+++ boards/mu/radio.js
@@ -1,2 +1,9 @@
 let songs = new Array("communism.mp3", "happy-2021.mp3", "happy-2022.mp3", "jacket-the-cat.mp3", "kill-it.mp3");
 
+let radio = docuemnt.getElementById('radio-src');
+
+radio.src = songs[1];
+
+radio.onended = (event) => {
+    radio.src = songs[2];
+};
