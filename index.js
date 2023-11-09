a34483f80f6d298ffcf801aaae0d4b8961e3ec9e
Timmothy Rain <ruben.p960@stu.nebo.edu>
2023-11-09 19:44:18 +0000

    Update file index.js

--- index.js
+++ index.js
@@ -135,10 +135,7 @@ boardID.on('open', error => {
     const color = "#" + boardC;
     // member element 
     const pastMember = document.createElement('div');
-    const pastImg = document.createElement('img');
-    pastImg.src = data;
     pastMember.appendChild(document.createTextNode(name));
-    pastMember.appendChild(pastImg);
     pastMember.className = 'member';
     pastMember.style.color = color;
     // message text element
